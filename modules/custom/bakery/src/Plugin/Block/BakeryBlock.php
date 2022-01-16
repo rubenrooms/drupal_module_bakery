e580de7ec4e0dbb7e20cbd614ad0ad956098c007
rubenrooms <ruben_rooms@hotmail.com>
2022-01-16 16:19:57 +0100

    add: block structure pt2

---
+++ modules/custom/bakery/src/Plugin/Block/BakeryBlock.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Drupal\bakery\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Defines a social menu block.
+ *
+ * @Block(
+ *  id = "bakery_block",
+ *  admin_label = @Translation("Bakery"),
+ * )
+ */
+class BakeryBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+
+  }
+
+}
