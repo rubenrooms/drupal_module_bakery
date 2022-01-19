5a7849d13892322e220ffe0fedba1aeaddee4750
rubenrooms <ruben_rooms@hotmail.com>
2022-01-19 14:13:07 +0100

    add: order options to form and database

--- modules/custom/bakery/src/Form/SettingsForm.php
+++ modules/custom/bakery/src/Form/SettingsForm.php
@@ -14,21 +14,72 @@ class SettingsForm extends FormBase {
 
     public function buildForm(array $form, FormStateInterface $form_state)
     {
+        $form['bread'] = [
+            '#type' => 'checkbox',
+            '#title' => ('Bread:'),
+            '#required' => false,
+        ];
+
+        $form['bread_type'] = [
+            '#type' => 'checkboxes',
+            '#title' => ('Type of bread:'),
+            '#required' => false,
+            '#options' => [
+                'white bread' => ('White bread'),
+                'grey bread' => ('Grey bread'),
+                'waldkorn bread' => ('Waldkorn bread'),
+                'ciabatta' => ('Ciabatta bread'),
+                'baguette' => ('Baguette')
+            ],
+            '#states' => [
+                'visible' => [
+                    ':input[name="bread"]' => [
+                        'checked' => true,
+                    ],
+                ],
+            ],
+        ];
+
+        $form['pastry'] = [
+            '#type' => 'checkbox',
+            '#title' => ('Pastry:'),
+            '#required' => false,
+        ];
+
+        $form['pastry_type'] = [
+            '#type' => 'checkboxes',
+            '#title' => ('Type of pastry:'),
+            '#required' => false,
+            '#options' => [
+                'pastry with chocolate and pudding' => ('Pastry with chocolate and pudding'),
+                'pastry with cherries' => ('Pastry with cherries'),
+                'croissant' => ('Croissant'),
+                'pistolet' => ('Pistolet')
+            ],
+            '#states' => [
+                'visible' => [
+                    ':input[name="pastry"]' => [
+                        'checked' => true,
+                    ],
+                ],
+            ],
+        ];
+
         $form['first_name'] = [
             '#type' => 'textfield',
-            '#title' => $this->t('First name:'),
+            '#title' => ('First name:'),
             '#required' => true,
         ];
 
         $form['last_name'] = [
             '#type' => 'textfield',
-            '#title' => $this->t('Last name:'),
+            '#title' => ('Last name:'),
             '#required' => true,
         ];
 
         $form['phone'] = [
             '#type' => 'tel',
-            '#title' => $this->t('Phone number:'),
+            '#title' => ('Phone number:'),
             '#required' => true,
         ];
 
@@ -43,9 +94,14 @@ class SettingsForm extends FormBase {
 
     public function submitForm(array &$form, FormStateInterface $form_state)
     {
+        \Drupal::state()->set('bakery.bread', $form_state->getValue('bread'));
+        \Drupal::state()->set('bakery.bread_type', $form_state->getValue('bread_type'));
+        \Drupal::state()->set('bakery.pastry', $form_state->getValue('pastry'));
+        \Drupal::state()->set('bakery.pastry_type', $form_state->getValue('pastry_type'));
+
         \Drupal::state()->set('bakery.first_name', $form_state->getValue('first_name'));
         \Drupal::state()->set('bakery.last_name', $form_state->getValue('last_name'));
         \Drupal::state()->set('bakery.phone', $form_state->getValue('phone'));
-        \Drupal::messenger()->addStatus('De gegevens zijn succesvol opgeslagen');
+        \Drupal::messenger()->addStatus('Your order is succesfully saved.');
     }
 }
